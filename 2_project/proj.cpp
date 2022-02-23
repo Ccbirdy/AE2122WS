ad42344d138710f700f8b7fffa26ad04f4ec879d
Chengcheng Guo <173-ge32lij@users.noreply.ci.inf-i2.uni-jena.de>
2022-02-23 21:28:31 +0100

    finish clean code

--- 2_project/proj.cpp
+++ 2_project/proj.cpp
@@ -29,7 +29,7 @@ int main() {
     //Write the following variables to the output file
     fout << magic_number << endl << pixel_per_row << " " << num_rows << endl << color_depth << endl;
     //Read in input file until file end putting values into appropriate variables
-    int i, j;
+    int i, j, k1, k2;
     vector<int> gray_pic(pixel_per_row*num_rows, 0);
     // convert rgb picture into gray scale
     for(i = 0; i < pixel_per_row*num_rows; i++ ){
@@ -41,29 +41,54 @@ int main() {
            
     cout<< "start pictrure enhancement" << endl;
     //Declare necessary variables for the adaptive_threshold_mean_C 
-    int filter_mask_size = 55, pad_num_rows, pad_pixel_per_row;     
+    int filter_mask_size = 21, C = 0, pad_num_rows, pad_pixel_per_row;     
     int offset = filter_mask_size/2;
-
+    // build filter mask
+    float filter_mask = 1.0 /(filter_mask_size*filter_mask_size);
+    vector<int> convolved_image(gray_pic.size(), 0);
+    // Explicitly zero-pad the original image
+    vector<int> pad_part(2*offset, 0);
+    vector<int> image_pad(offset*pad_pixel_per_row + offset , 0);
+    for( i = 0; i<num_rows; i++) {
+        for(j =0; j< pixel_per_row; j++) {
+            image_pad.push_back(gray_pic[j + i*pixel_per_row]);
+        }
+        image_pad.insert(image_pad.end(), pad_part.begin(), pad_part.end());
+    }
+    for(i = 0; i< offset*pad_pixel_per_row - offset; i++) {
+        image_pad.push_back(0);
+    }
     
 
+    // Convolve
+    for(j=0; j< num_rows; j++){
+        int local_sum = 0;
+        for(i = 0; i< pixel_per_row; i++){
+            for ( k1 = 0; k1< offset; k1++){
+                for ( k2 = 0; k2< offset; k2++){
+                    local_sum += image_pad[i + j*pixel_per_row + k2 +k1*pixel_per_row];
+                }
+            }
+            local_sum = local_sum * filter_mask;
+            local_sum = local_sum - C;
+
+            if (gray_pic[i+j*pixel_per_row] > local_sum){
+            convolved_image[i+j*pixel_per_row] = color_depth;            
+            }
+            else{
+                convolved_image[i+j*pixel_per_row] = 0;
+            }
+        }        
+    }
 
 
-
-
-
-
-
-    
+    cout << omp_get_wtime() - start << " seconds" << endl;
     cout<< "start writing" << endl;
-    // // Explicitly zero-pad the gray image
-    // pad_num_rows = num_rows + 2*offset;
-    // pad_pixel_per_row = pixel_per_row + 2*offset;    
-    // vector<int> pad_pic(pad_pixel_per_row*offset*2, 0);
 
     //Write vector to output file
     for(i = 0; i < pixel_per_row*num_rows; i++ ){
         // //Write converted values to output file
-        fout << gray_pic[i] << " "<< gray_pic[i] << " "<< gray_pic[i] << endl;            
+        fout << convolved_image[i] << " "<< convolved_image[i] << " "<< convolved_image[i] << endl;            
     }   
  
     cout << omp_get_wtime() - start << " seconds" << endl;
