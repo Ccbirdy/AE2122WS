d411c65d26cc5f529dfe3edd37c08949c87c8bbc
Chengcheng Guo <173-ge32lij@users.noreply.ci.inf-i2.uni-jena.de>
2022-02-24 12:26:40 +0100

    speed up the write ouput file 70s --> 5s

--- 2_project/proj.cpp
+++ 2_project/proj.cpp
@@ -12,8 +12,8 @@ using namespace std;
 
 int main() {
     //File input and output streams
-    ifstream fin("out.ppm");
-    ofstream fout("output.ppm");
+    std::ifstream fin("out.ppm");
+    std::ofstream fout("output.ppm");
     //Check if input file was successfully opened
     if (!fin) {
         cout << "Error - Nonexistent Image (.ppm) File" << endl;
@@ -23,23 +23,26 @@ int main() {
     const double start = omp_get_wtime();
     //Declare necessary variables
     string magic_number;
-    int pixel_per_row, num_rows, color_depth, red, green, blue;
+    int pixel_per_row, num_rows, color_depth, red, green, blue, gray;
     //Read in values for the following variables from input file
     fin >> magic_number >> pixel_per_row >> num_rows >> color_depth;
     //Write the following variables to the output file
-    fout << magic_number << endl << pixel_per_row << " " << num_rows << endl << color_depth << endl;
-    //Read in input file until file end putting values into appropriate variables
+    fout << magic_number << endl;
+    fout << pixel_per_row << " " << num_rows << endl << color_depth << endl;
+    //Read in input file convert it in gray scale and save in vector
     int i, j, k1, k2, local_sum;
     vector<int> gray_pic(pixel_per_row*num_rows, 0);
+#pragma omp ordered 
     // convert rgb picture into gray scale
     for(i = 0; i < pixel_per_row*num_rows; i++ ){
         fin >> red >> green >> blue;
         //Covert each pixel to grayscale
-        red = int(0.2989 * red + 0.5870 * green + 0.1140 * blue);         
-        gray_pic[i]=red;
+        gray = int(0.2989 * red + 0.5870 * green + 0.1140 * blue);         
+        gray_pic[i]=gray;
     }    
-           
+    cout << "finish generate gray_sacle_image: " << omp_get_wtime() - start << " seconds" << endl;
     cout<< "start pictrure enhancement" << endl;
+
     //Declare necessary variables for the adaptive_threshold_mean_C 
     int filter_mask_size = 55, C = 10, pad_num_rows, pad_pixel_per_row;     
     int offset = filter_mask_size/2;
@@ -49,49 +52,55 @@ int main() {
     // Explicitly zero-pad the original image
     vector<int> pad_part(2*offset, 0);
     vector<int> image_pad(offset*pad_pixel_per_row + offset , 0);
-    for( i = 0; i<num_rows; i++) {
-        for(j =0; j< pixel_per_row; j++) {
-            image_pad.push_back(gray_pic[j + i*pixel_per_row]);
+
+    for( j = 0; j<num_rows; j++) {
+        for(i =0; i< pixel_per_row; i++) {
+            image_pad.push_back(gray_pic[i + j*pixel_per_row]);
         }
         image_pad.insert(image_pad.end(), pad_part.begin(), pad_part.end());
     }
+#pragma omp ordered 
     for(i = 0; i< offset*pad_pixel_per_row - offset; i++) {
         image_pad.push_back(0);
-    }
-    
+    }    
 
-    // Convolve rhe padded image with filtermask (mean filter)
+    // Convolve
+#pragma omp parallel for collapse(2) private(local_sum) 
+
     for(j=0; j< num_rows; j++){        
         for(i = 0; i< pixel_per_row; i++){
+
             local_sum = 0;
+// #pragma omp parallel for reduction(+ : local_sum)// not wrong but no use
 
             for ( k1 = 0; k1< filter_mask_size; k1++){
-                for ( k2 = 0; k2< filter_mask_size; k2++){
+                for ( k2 = 0; k2< filter_mask_size; k2++){                    
                     local_sum += image_pad[i + j*pixel_per_row + k2 +k1*pixel_per_row];
                 }
             }
-            local_sum = local_sum * filter_mask - C;  
-            fout << local_sum << " ";        
+
+//#pragma omp taskwait
+            local_sum = local_sum * filter_mask -C;                 
+        
             if (gray_pic[i+j*pixel_per_row] > local_sum){
-                convolved_image[i+j*pixel_per_row] = color_depth;            
+            convolved_image[i+j*pixel_per_row] = color_depth;            
             }
             else{
                 convolved_image[i+j*pixel_per_row] = 0;
             }
         }        
-    }
+    }    
 
-
-    cout << omp_get_wtime() - start << " seconds" << endl;
+    
+    cout << "finish calculate: " << omp_get_wtime() - start << " seconds" << endl;
     cout<< "start writing" << endl;
 
     //Write vector to output file
-    for(i = 0; i < pixel_per_row*num_rows; i++ ){
-        // //Write converted values to output file
-        fout << convolved_image[i] << " "<< convolved_image[i] << " "<< convolved_image[i] << endl;            
-    }   
- 
-    cout << omp_get_wtime() - start << " seconds" << endl;
+    for(const auto &e : convolved_image) fout << e << " "<< e << " "<< e << " ";
+
+
+    cout << "finish writing: " << omp_get_wtime() - start << " seconds" << endl;
+
     //Close files
     fin.close();
     fout.close();
