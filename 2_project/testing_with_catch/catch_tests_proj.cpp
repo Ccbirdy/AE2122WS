2d9659f72ee21feb7954f8945b7c48b9b947ef91
Chengcheng Guo <173-ge32lij@users.noreply.ci.inf-i2.uni-jena.de>
2022-03-01 00:31:09 +0100

    test with catch, not finished

---
+++ 2_project/testing_with_catch/catch_tests_proj.cpp
@@ -0,0 +1,12 @@
+#include "catch.h"
+
+#include <algorithm>
+
+// the TEST_CASE macro defines a test case, called "computation of pi" 
+// as a label for the test case we use [correctness] 
+// TEST_CASE("generate an enhance picture", "[correctness]") {
+
+//   // expression in REQUIRE must evaluate to true or the test fails 
+//   REQUIRE(*max_element(gray_pic.begin(), gray_pic.end()) > 0) ;
+// }
+
