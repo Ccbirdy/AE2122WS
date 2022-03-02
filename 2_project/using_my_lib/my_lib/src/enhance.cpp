3fbf6a215779447566645a6ba30fd9c55d9b0dd9
Chengcheng Guo <173-ge32lij@users.noreply.ci.inf-i2.uni-jena.de>
2022-03-02 02:56:05 +0100

    more functions

--- 2_project/using_my_lib/my_lib/src/enhance.cpp
+++ 2_project/using_my_lib/my_lib/src/enhance.cpp
@@ -1,5 +1,25 @@
 #include "enhance.h"
+#include <vector>
 
 int to_gray(int red, int green, int blue){
     return int(0.2989 * red + 0.5870 * green + 0.1140 * blue);
+}
+
+
+std::vector<int>& generate_pad (std::vector<int>& gray_pic, std::vector<int>& image_pad, std::vector<int>& pad_part, 
+                    int offset, int num_rows, int pixel_per_row, int pad_pixel_per_row) {
+    int i, j;
+
+//#pragma omp ordered 
+    for( j = 0; j<num_rows; j++) {
+        for(i =0; i< pixel_per_row; i++) {
+            image_pad.push_back(gray_pic[i + j*pixel_per_row]);
+        }
+        image_pad.insert(image_pad.end(), pad_part.begin(), pad_part.end());
+    }
+//#pragma omp ordered 
+    for(i = 0; i< offset*pad_pixel_per_row - offset; i++) {
+        image_pad.push_back(0);
+    }  
+
 }
